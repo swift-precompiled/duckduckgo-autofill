a18999b303322ffe5a4b97466421bf8f3663a2fd
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-13 00:22:21 +0530

    Updated for release 19.2.0

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.9.0
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "duckduckgo-autofill",
+    products: [
+        .library(
+            name: "Autofill",
+            targets: ["Autofill_PrecompiledProduct"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Autofill_Aggregation",
+            dependencies: ["Autofill"],
+            swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
+        ),
+        .binaryTarget(
+            name: "Autofill",
+            url: "https://github.com/swift-precompiled/duckduckgo-autofill/releases/download/19.2.0/Autofill-83ddb66f0d21da67ea814511f9b59609bc40e7710d6acf3efeeb94466d172c87.xcframework.zip",
+            checksum: "83ddb66f0d21da67ea814511f9b59609bc40e7710d6acf3efeeb94466d172c87"
+        ),
+        .target(
+            name: "AutofillResources_Aggregation",
+            dependencies: ["AutofillResources"],
+            swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
+        ),
+        .binaryTarget(
+            name: "AutofillResources",
+            url: "https://github.com/swift-precompiled/duckduckgo-autofill/releases/download/19.2.0/AutofillResources-3f922852c024c1c4f142b3a53123b80b5dfc20541a44e60d4d99290d20b37767.xcframework.zip",
+            checksum: "3f922852c024c1c4f142b3a53123b80b5dfc20541a44e60d4d99290d20b37767"
+        ),
+        .target(
+            name: "Autofill_PrecompiledProduct",
+            dependencies: ["Autofill_Aggregation", "AutofillResources_Aggregation"]
+        )
+    ]
+)
